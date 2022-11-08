4adb018034dddb7b21eb09d3bfed0c6af8f568e6
akash.kaintura <akash.kaintura@srijan.net>
2022-11-08 12:23:37 +0530

    update: app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,22 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OverviewComponent } from './admin/overview/overview.component';
-import { BlogEntryComponent } from './components/blog-entry/blog-entry.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { UserComponent } from './components/user/user.component';
+import { OverviewComponent } from './admin/components/overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent,
-    BlogEntryComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    UserComponent
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
